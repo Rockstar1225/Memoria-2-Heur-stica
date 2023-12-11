647428853b452d868a08fee3b15a53a15a21bc8f
ALVAROPING1 <alvaroping1@gmail.com>
2023-12-11 14:25:59 +0100

    añadidos colores a los mensajes de error

--- parte-2/astar/src/main.rs
+++ parte-2/astar/src/main.rs
@@ -4,11 +4,11 @@ use astar::Config;
 
 fn main() {
     let config = Config::build(env::args()).unwrap_or_else(|err| {
-        println!("Error parsing arguments: {err}");
+        println!("\x1B[31mError Parsing Arguments:\x1B[0m {err}");
         process::exit(1);
     });
     if let Err(e) = astar::run(&config) {
-        println!("Application error: {e}");
+        println!("\x1B[31mApplication Error:\x1B[0m {e}");
         process::exit(1);
     }
 }
